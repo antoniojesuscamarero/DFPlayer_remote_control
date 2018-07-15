10eec8991b3608648bc833bab1ac9c6fe766fbbe
antoniojesuscamarero <jesesad@gmail.com>
2018-07-15 21:43:53 +0200

    Update DFplayer_remote.ino

--- DFplayer_remote.ino
+++ DFplayer_remote.ino
@@ -1,3 +1,17 @@
+/***************************************************
+DFPlayer - A Mini MP3 Player For Arduino
+ <https://www.dfrobot.com/index.php?route=product/product&product_id=1121>
+ 
+ ***************************************************
+ This example shows the basic function of library for DFPlayer.
+ 
+ Created 2016-12-07
+ By [Angelo qiao](Angelo.qiao@dfrobot.com)
+ 
+ GNU Lesser General Public License.
+ See <http://www.gnu.org/licenses/> for details.
+ All above must be included in any redistribution
+ ****************************************************/
 #include "SoftwareSerial.h"
 #include "DFRobotDFPlayerMini.h"
 #include <Arduino.h>
